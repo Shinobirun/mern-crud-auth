ad9b8297ab85c562280daccf7249f54d3b2fe864
shinobirun <adrian.latorre@gmail.com>
2024-02-04 10:34:39 -0300

    terminamos el login y el logout

--- src/routes/auth.rotues.js
+++ src/routes/auth.rotues.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {login, register} from '../controllers/auth.comtrolers.js'
+import {login, register,logout} from '../controllers/auth.comtrolers.js'
 
 
 const router = Router()
@@ -7,5 +7,7 @@ const router = Router()
 router.post('/register', register);
 
 router.post('/login', login);
+
+router.post('/logout',logout);
 
 export default router;
