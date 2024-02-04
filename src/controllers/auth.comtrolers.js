237c73034b07d4bcf884c7efb29b33a790abbcce
shinobirun <adrian.latorre@gmail.com>
2024-02-04 07:47:13 -0300

    logramos el token

--- src/controllers/auth.comtrolers.js
+++ src/controllers/auth.comtrolers.js
@@ -1,19 +1,40 @@
 import   User   from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const register = async (req, res) => {
     const {username,email,password} = req.body
 
    try {
+
+    const passwordHash = await bcrypt.hash(password,10)
+
     const newUser = new User({
         username,
         email,
-        password
+        password: passwordHash,
     });
 
     const userSaved = await newUser.save();
 
-    res.json(userSaved);
-   } catch (error) {
+    jwt.sign({
+        id: userSaved._id
+    }, 'secret123', {
+        expiresIn: '1d'
+    }, (err, token) => {
+        if (err) console.log(err);
+        res.json({ token });
+    });
+
+   // res.json({
+   //     id: userSaved._id,
+   //     username: userSaved.username,
+   //     email: userSaved.email,
+   //     creatdAt: userSaved.createdAt,
+   //     updatedAt:userSaved.updatedAt,
+   // });
+   }
+    catch (error) {
     console.log(error)
     
    }
