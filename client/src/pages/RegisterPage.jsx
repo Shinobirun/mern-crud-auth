1f796b3fb6f0be6533404c0e55feb77846630980
shinobirun <adrian.latorre@gmail.com>
2024-02-18 18:41:20 -0300

    agregamos contexto

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,18 @@
-import { useForm } from 'react-hook-form'
-import {registerRequest} from '../api/auth'
+import { useForm } from 'react-hook-form';
+import { useAuth} from '../context/AuthContext';
+
 
 function RegisterPage()  {
 
-    const {register, handleSubmit}= useForm()
+    const {register, handleSubmit}= useForm();
+    const {signup, user} = useAuth();
+
+    console.log(user)
+
+
     const onSubmit = handleSubmit(async (values) =>{
-        
-        const res = await registerRequest(values)
-        console.log(res);
-    })
+        signup(values); 
+    });
 
 
     return (
